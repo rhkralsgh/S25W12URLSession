e0491ceb03731edd4e046828503b8f38b53865e1
rhkralsgh <knh239@naver.com>
2025-11-24 16:13:48 +0900

    🔐API Key

---
+++ .gitignore.swift
@@ -0,0 +1,1 @@
+SongApiConfig.swift
---
+++ S25W12URLSession/Api/SongApiConfig.example.swift
@@ -0,0 +1,9 @@
+// SongAPIConfig.swift
+
+//struct SongApiConfig {
+struct SongApiConfigExample {
+    // Supabase의 connect 정보
+    static let projectURL = "https://mcrum~~~~~.supabase.co"
+    static let apiKey = "eyJhb~~~~~~~~~~~~~~~~~~~~~~~~~~~~~.eyJ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~.~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    static let serverURL = "\(projectURL)/rest/v1/songs?apikey=\(apiKey)"
+}
